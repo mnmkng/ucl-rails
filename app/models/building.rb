3e992de2d00a5ed9191d2928f6c135d8e8905123
Ondrej Urban <ondrej.urban@plus4u.net>
2017-12-17 12:07:32 +0100

    add lesson list filtering and frontend

--- app/models/building.rb
+++ app/models/building.rb
@@ -15,8 +15,20 @@ class Building < ApplicationRecord
   validates :title, presence: true
   validates :code, presence: true
 
-  def get_lessons
+  def list_lessons
+    lessons = []
+    self.rooms.each do |room|
+      room.lessons.each do |lesson|
+        lessons << lesson if today?(lesson)
+      end
+    end
+    lessons.sort_by {|l| l.start_at}
+  end
 
+  # hardcoded today to match seed data for project evaluation
+  def today?(lesson)
+    today = Date.new(2017, 12, 15)
+    lesson.start_at.to_date == today || lesson.end_at.to_date == today
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :students
     resources :teachers
 
-    root to: 'dashboard#index'
+    root to: 'dashboard#show'
   end
 
   resources :buildings
